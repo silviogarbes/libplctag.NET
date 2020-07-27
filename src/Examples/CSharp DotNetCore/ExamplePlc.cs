3d86de9949e4a53fe422d8f2e9c4d7c53baecfa5
timyhac <timyhac@gmail.com>
2020-07-28 09:55:50 +1000

    Added Plc object

---
+++ src/Examples/CSharp DotNetCore/ExamplePlc.cs
@@ -0,0 +1,37 @@
+﻿using libplctag;
+using System;
+using System.Net;
+using System.Threading;
+
+namespace CSharpDotNetCore
+{
+    class ExamplePlc
+    {
+        public static void Run()
+        {
+
+            const int TIMEOUT = 5000;
+
+            var myPlc = new Plc()
+            {
+                Gateway = "192.168.0.10",
+                Path = "1,0",
+                Type = PlcType.ControlLogix
+            };
+
+            var myTag = myPlc.CreateTag("PROGRAM: SomeProgram.SomeDINT", DataType.DINT);
+            
+            myTag.Initialize(TIMEOUT);
+
+            myTag.SetInt32(0, 3737);
+
+            myTag.Write(TIMEOUT);
+
+            myTag.Read(TIMEOUT);
+
+            int myDint = myTag.GetInt32(0);
+
+            Console.WriteLine(myDint);
+        }
+    }
+}
---
+++ src/libplctag/Plc.cs
@@ -0,0 +1,48 @@
+﻿using libplctag.Generic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace libplctag
+{
+    public class Plc
+    {
+        public string Gateway { get; set; }
+        public string Path { get; set; }
+        public PlcType? Type { get; set; }
+        public bool? UseConnectedMessaging { get; set; }
+
+        /// <summary>
+        /// A factory for Tag objects that injects properties related to a Plc connection into the Tag.
+        /// </summary>
+        public Tag CreateTag(string name, int elementSize, int elementCount = 1)
+        {
+            return new Tag()
+            {
+                Name = name,
+                ElementSize = elementSize,
+                ElementCount = elementCount,
+                Gateway = Gateway,
+                PlcType = Type,
+                UseConnectedMessaging = UseConnectedMessaging,
+            };
+        }
+
+        /// <summary>
+        /// A factory for GenericTag objects that injects properties related to a Plc connection into the Tag.
+        /// </summary>
+        public GenericTag<TPlcType, TDotNetType> CreateGenericTag<TPlcType, TDotNetType>(string name, int elementCount = 1)
+            where TPlcType : IPlcType<TDotNetType>, new()
+        {
+            return new GenericTag<TPlcType, TDotNetType>()
+            {
+                Name = name,
+                ElementCount = elementCount,
+                Gateway = Gateway,
+                PlcType = Type,
+                UseConnectedMessaging = UseConnectedMessaging
+            };
+        }
+
+    }
+}
